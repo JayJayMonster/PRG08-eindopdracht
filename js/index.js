b8ba4ffe4a35a83d89ac382d43b5564843587e36
Jay <jayvanherk@gmail.com>
2022-04-04 11:24:36 +0200

    puntjes op de i

--- js/index.js
+++ js/index.js
@@ -17,8 +17,18 @@ const fileButton = document.getElementById('file');
 const button = document.getElementById('btn');
 const accuracy = document.getElementById('accuracy');
 const testImg = document.getElementById('output');
-let synth = window.speechSynthesis;
+const synth = window.speechSynthesis;
 const labelsArray = ['Cardboard', 'Glass', 'Metal', 'Paper', 'Plastic'];
+const recycle = document.getElementById('recycle');
+
+//variables recycling
+const recycleObject = {
+  Cardboard: 'You can use cardboard for something',
+  Glass: 'There is a special bin for glass',
+  Metal: 'Maybe use this metal to something something',
+  Paper: 'Recycle your paper',
+  Plastic: 'Horrible for the environment',
+};
 
 //* event listeners
 button.addEventListener('click', classify);
@@ -39,11 +49,12 @@ function classify() {
   featureExtractor.classify(
     document.getElementById('output'),
     (err, result) => {
-      console.log(result);
+      ////console.log(result);
       accuracy.innerText = `I am ${Math.round(
         result[0].confidence * 100
       )}% sure it is ${labelsArray[result[0].label]}`;
       speak(accuracy.innerHTML);
+      recycle.innerText = recycleObject[labelsArray[result[0].label]];
     }
   );
 }
